cd642b8597bded0d76ba8021f41825070205228e
Benjamin Croker <benjamin.croker@gmail.com>
2013-11-27 21:21:08 +1000

    Added PU lines as csv file and csv reader dependency

--- project.clj
+++ project.clj
@@ -10,7 +10,8 @@
                  [ring/ring-json "0.2.0"]
                  [ring "1.1.8"]
                  [org.clojure/data.json "0.2.2"]
-                 [postgresql "9.1-901.jdbc4"]]
+                 [postgresql "9.1-901.jdbc4"]
+                 [org.clojure/data.csv "0.1.2"]]
   :plugins [[lein-ring "0.8.5"]]
   :min-lein-version "2.0.0"
   :main bplaas.web
