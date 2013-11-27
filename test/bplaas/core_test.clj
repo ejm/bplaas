2ad71cf9e8c91252bc455a070f2213d529297f31
Hugo <hugo.firth@gmail.com>
2013-11-27 21:07:28 +1000

    Hugo - got basic integration test passing

--- test/bplaas/core_test.clj
+++ test/bplaas/core_test.clj
@@ -4,23 +4,22 @@
             [bplaas.core :refer :all]
             [clojure.java.jdbc :as sql]))
 
-(facts "database-url-test"
-       (with-state-changes 
-         [
-          (before :facts 
-                  (do
-                    (sql/with-connection (System/getenv "DATABASE_URL")
-                      (sql/create-table :testing [:data :text]))
-                    (sql/with-connection (System/getenv "DATABASE_URL")
-                      (sql/insert-record :testing {:data "Hello World"}))))
-          (after :facts
-                 (sql/with-connection (System/getenv "DATABASE_URL")
-                   (sql/drop-table :testing))      )])
-       
+(with-state-changes
+  [
+   (before :facts 
+           (sql/with-connection (System/getenv "DATABASE_URL")
+             (sql/create-table :testing [:data :text])))
+   (before :facts 
+           (sql/with-connection (System/getenv "DATABASE_URL")
+             (sql/insert-record :testing {:data "Hello World"})))
+   (after :facts 
+          (sql/with-connection (System/getenv "DATABASE_URL")
+            (sql/drop-table :testing)))
+   ]
   (fact "Test a connection can be made to the database"
         (sql/with-connection (System/getenv "DATABASE_URL")
           (sql/with-query-results results
             ["select * from testing"]
             (into [] results))) => [{:data "Hello World"}]
-          )
-)
+            )
+         )
