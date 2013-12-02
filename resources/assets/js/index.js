948c8d96f51fed30f0803cd2f7064f4e9e502e76
Hugo <hugo.firth@gmail.com>
2013-12-02 20:28:16 +1000

    Hugo - now updating the page when you click the link

---
+++ resources/assets/js/index.js
@@ -0,0 +1,15 @@
+
+
+function updatePickupLine() {
+    var update = function (text) {
+        $('#pickupLine').text(text);
+    }
+    getPickupLine(update);
+}
+
+function getPickupLine(element) {
+    $.getJSON( "/pickup-line", function( data ) {
+        
+        element(data.pickup_line);
+    });
+}
